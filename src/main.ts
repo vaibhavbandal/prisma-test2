5a717bfedf57922dc1e3ffe412c2178b1fc899ba
vaibhavbandal <86363586+vaibhavbandal@users.noreply.github.com>
2022-11-06 11:38:18 +0530

    hello

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');  
 
-  await app.listen(3000);
+  await app.listen(3000);  
   
  console.info('server started at 3000')
  
